d220add0914f0a8b3e5c9c2910cca21c1316f0fd
Pawel <146331117+SolPaw321@users.noreply.github.com>
2025-07-26 20:25:38 +0200

    Delete TestMesh directory

--- TestMesh/main.py
+++
@@ -1,9 +0,0 @@
-from Meshing import Meshing
-
-
-meshing = Meshing()
-meshing.get_all_parts()
-meshing.create_size_control("CURVATURE")
-meshing.create_size_control("BOI")
-meshing.plot()
-meshing.exit()
--- TestMesh/misc/PATHS.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-TestMesh = Path(__file__).resolve().parent.parent
-
-Model_stp_file = TestMesh / 'model' / 'Wind_Turbine.stl'
-
-# ANSYS_Prime = r"D:\programy\ansys\ANSYS Inc\v251\meshing\Prime"
-ANSYS_Prime = r"C:\Program Files\ANSYS Inc\v251\meshing\Prime"
