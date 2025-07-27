136e77d6a7350dacfc390657d081bc377b242361
Pawel <146331117+SolPaw321@users.noreply.github.com>
2025-07-27 23:13:51 +0200

    some changes
    some changes

---
+++ model/D2_5/geometry/Airfoil.py
@@ -0,0 +1,135 @@
+from beartype import beartype as check_input_types
+from ansys.geometry.core.math import Point2D
+from numpy import sin, cos, arctan, pi
+
+
+class Airfoil:
+    """
+    Airfoil class description.
+    """
+    @check_input_types
+    def __init__(self, number: str | int, n_points: str | int = 200):
+        self.__validate_number(number)
+        self.__validate_n_points(n_points)
+        self.number = int(number)
+        self.n_points = int(n_points)
+
+        self.m, self.p, self.t = None, None, None
+        self.__calculate_naca_digits()
+
+        self._points = []
+
+    @staticmethod
+    def __validate_number(number):
+        """
+        Validate "number" parameter.
+
+        Args:
+            number (str | int): The number of points that make up a NACA airfoil.
+
+        Raise:
+            TypeError: number is not str or int
+            ValueError: number is not 4-digit
+        """
+        if not isinstance(number, str | int):
+            raise TypeError(f"\'number\' parameter should be str or int, got {type(number)}")
+        if not len(str(number)) == 4:
+            raise ValueError(f"expected 4 digit (eq. 1234), got {len(str(number))} {number}")
+
+    @staticmethod
+    def __validate_n_points(n_points):
+        """
+        Validate "n_points" parameter.
+
+        Args:
+            n_points (str | int): The number of points that make up a NACA airfoil.
+
+        Raise:
+            TypeError: n_points is not str or int
+            ValueError: n_points is not positive
+        """
+        if not isinstance(n_points, str | int):
+            raise TypeError(f"\'n_points\' parameter should be str or int, got {type(n_points)}")
+        if not int(n_points) > 0:
+            raise ValueError(f"\'n_points\' parameter should be positive, got {n_points}")
+
+    def __calculate_naca_digits(self):
+        """
+        Calculate NACA 4-digits parameters.
+        """
+        self.m = self.number // 1000 * 0.01
+        self.p = self.number // 100 % 10 * 0.1
+        self.t = self.number % 100 * 0.01
+
+    def generate_points(self) -> list[Point2D]:
+        """
+            Generate a NACA 4-digits airfoil.
+
+            Return:
+                list[Point2D]: List of points that define the airfoil
+            """
+        for i in range(self.n_points):
+
+            # Make it an exponential distribution so the points are more concentrated
+            # near the leading edge
+            x = (1 - cos(i / (self.n_points - 1) * pi)) / 2
+
+            # Check if it is a symmetric airfoil
+            if self.p == 0 and self.m == 0:
+                # Camber line is zero in this case
+                yc = 0
+                dyc_dx = 0
+            else:
+                # Compute the camber line
+                if x < self.p:
+                    yc = self.m / self.p ** 2 * (2 * self.p * x - x ** 2)
+                    dyc_dx = 2 * self.m / self.p ** 2 * (self.p - x)
+                else:
+                    yc = self.m / (1 - self.p) ** 2 * ((1 - 2 * self.p) + 2 * self.p * x - x ** 2)
+                    dyc_dx = 2 * self.m / (1 - self.p) ** 2 * (self.p - x)
+
+            # Compute the thickness
+            yt = 5 * self.t * (0.2969 * x ** 0.5
+                               - 0.1260 * x
+                               - 0.3516 * x ** 2
+                               + 0.2843 * x ** 3
+                               - 0.1015 * x ** 4)
+
+            # Compute the angle
+            theta = arctan(dyc_dx)
+
+            # Compute the points (upper and lower side of the airfoil)
+            xu = x - yt * sin(theta)
+            yu = yc + yt * cos(theta)
+            xl = x + yt * sin(theta)
+            yl = yc - yt * cos(theta)
+
+            # Append the points
+            self._points.append(Point2D([xu, yu]))
+            self._points.insert(0, Point2D([xl, yl]))
+
+            # Remove the first point since it is repeated
+            if i == 0:
+                self._points.pop(0)
+        self._points.append(self._points[0])
+        return self._points
+
+    @property
+    def points(self) -> list[Point2D]:
+        """
+        Return List of points that define the airfoil.
+
+        Args:
+            list[Point2D]: List of points that define the airfoil
+        """
+        return self._points
+
+    @points.setter
+    def points(self, points: list[Point2D]):
+        """
+        Set new points.
+
+        Args:
+            points (list[Point2D]): list of new points to set
+        """
+        self._points = points
---
+++ model/D2_5/geometry/misc/points.py
@@ -0,0 +1,81 @@
+from ansys.geometry.core.math import Point2D
+from math import radians, cos, sin
+from ansys.geometry.core.misc import Distance, Angle
+from model.D2_5.geometry.Airfoil import Airfoil
+
+
+def translate_airfoil_on_circle(foil: Airfoil,
+                                center: Point2D,
+                                radius: Distance,
+                                angle_deg: Angle,
+                                angle_of_attack_deg: Angle) -> Airfoil:
+    """
+    Place an Airfoil on a circle.
+
+    Args:
+        foil (Airfoil): Airfoil with list of points
+        center (Point2D): center of circle
+        radius (Distance): radius of circle
+        angle_deg (Angle): interior angle (degrees)
+        angle_of_attack_deg (Angle): angle of attack (degrees)
+    Return:
+        Airfoil: Airfoil with set new points
+    """
+    points = foil.points
+    cx = center.x + radius.value * cos(radians(angle_deg.value.m))
+    cy = center.y + radius.value * sin(radians(angle_deg.value.m))
+    new_center = Point2D([cx.m, cy.m])
+
+    translated = []
+    for p in points:
+        local = Point2D([p.x.m - 0.5, p.y.m])
+
+        local_rotated = __points_rotation(local, Point2D([0, 0]), angle_of_attack_deg)
+
+        shifted = Point2D([local_rotated.x.m + new_center.x.m, local_rotated.y.m + new_center.y.m])
+
+        final = __points_rotation(shifted, new_center, angle_deg)
+        translated.append(final)
+
+    foil.points = translated
+
+    return foil
+
+
+def __points_rotation(points: list[Point2D] | Point2D,
+                      center: Point2D,
+                      angle: Angle) -> list[Point2D] | Point2D:
+    """
+    Rotate a list of points (or a single point) around center by angle.
+
+    Args:
+        points (list[Point2D] | Point2D): list of points or single point to rotate
+        center (Point2D): the center of rotation
+        angle (Angle): the angle (degrees) to rotate
+    Return:
+        list[Point2D] | Point2D: list of rotated points or single rotated point
+    """
+    # set type of return
+    if isinstance(points, Point2D):
+        points = [points]
+        list_return = False
+    else:
+        list_return = True
+
+    new_points = []
+    angle_rad = radians(angle.value.m)
+    for point in points:
+        translated_x = point.x - center.x
+        translated_y = point.y - center.y
+
+        rotated_x = translated_x * cos(angle_rad) - translated_y * sin(angle_rad)
+        rotated_y = translated_x * sin(angle_rad) + translated_y * cos(angle_rad)
+
+        new_x = rotated_x + center.x
+        new_y = rotated_y + center.y
+
+        new_points.append(Point2D([new_x.m, new_y.m]))
+
+    if list_return:
+        return new_points
+    return new_points[0]
