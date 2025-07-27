74738d31c0858f44ea083aa4c358ec773c50e554
Pawel <146331117+SolPaw321@users.noreply.github.com>
2025-07-27 23:12:49 +0200

    Delete model/D2_5/geometry directory

--- model/D2_5/geometry/AirfoilLoad.py
+++
@@ -1,8 +0,0 @@
-from misc.PATHS import AIRFOIL_MODEL
-
-
-class AirfoilLoad:
-    def __init__(self):
-        pass
-
-    def __load_body(self):
--- model/D2_5/geometry/misc/PATHS.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-GEOMETRY = Path(__file__).resolve().parent.parent
-
-RESULTS = GEOMETRY / "results"
-
-RESULTS.mkdir() if not RESULTS.is_dir() else None
-
-AIRFOIL_MODEL = GEOMETRY / "airfoil_model" / "airfoil6412.step"
